492862b8529a4118b3f64a1e32b52c16f2282e0d
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-09-06 01:45:34 +1000

    updated handlebars to show new edit functionality

--- public/js/editblog.js
+++ public/js/editblog.js
@@ -1,15 +1,25 @@
 const updateButtonHandler = async (event) => {
+  event.preventDefault();
+
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
+    const title = document.querySelector('#blog-name').value.trim();
+    const content = document.querySelector('#blog-desc').value.trim();
 
-    const response = await fetch(`/api/blog/${id}`, {
-      method: 'PUT',
-    });
+      if (title && content) {
+        const response = await fetch(`/api/blog/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to update project');
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update project');
+      }
     }
   }
 };
